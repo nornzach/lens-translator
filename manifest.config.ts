a079fed7d54ebc8da1cbb5eae2aaa5288090f3cb
Zach <kny4wy47tn@privaterelay.appleid.com>
2026-07-12 02:13:26 +0800

    chore: bump extension to 0.1.2 for clean reinstall

--- manifest.config.ts
+++ manifest.config.ts
@@ -4,7 +4,7 @@ export default defineManifest({
   manifest_version: 3,
   name: 'Lens Translator',
   description: 'Hold a hotkey to peek Chinese translations without leaving English immersion.',
-  version: '0.1.1',
+  version: '0.1.2',
   action: {
     default_popup: 'src/popup/index.html',
     default_title: 'Lens Translator',
